05085eaf7482d0c9d859eba49a74b8ac483536d4
Alberto Barboza <73672526+Albertobar94@users.noreply.github.com>
2023-10-06 20:27:07 +0200

    Refactoring chat gipityyy

--- hello-art/src/services/chatgipity/gipity.ts
+++ hello-art/src/services/chatgipity/gipity.ts
@@ -1,29 +1,31 @@
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
-import { z } from "zod";
+import { z } from 'zod';
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 const params = z.object({
-    messages: z.array(
-      z.object({
-        role: z.enum(["function", "system", "user", "assistant"]),
-        content: z.string(),
-      })
-    ),
-  });
+  messages: z.array(
+    z.object({
+      role: z.enum(['function', 'system', 'user', 'assistant']),
+      content: z.string(),
+    }),
+  ),
+});
 
-export async function runGipity(mgs: z.infer<typeof params>) {
+export const Gipity = {
+  async execute(mgs: z.infer<typeof params>) {
     const { messages } = params.parse(mgs);
 
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages,
-  });
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      stream: true,
+      messages,
+    });
 
-  const stream = OpenAIStream(response);
-  return new StreamingTextResponse(stream);
-}
+    const stream = OpenAIStream(response);
+    return new StreamingTextResponse(stream);
+  },
+};
