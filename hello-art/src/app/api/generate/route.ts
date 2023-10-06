a9d8ec8bae2d472aa9256e2aaccc3a10e7812104
Matěj Podzemný <matej@oakslab.com>
2023-10-06 18:40:29 +0200

    feat: init prettier (#1)
    * init prettier
    
    * Update .gitignore
    
    * Delete .env

--- hello-art/src/app/api/generate/route.ts
+++ hello-art/src/app/api/generate/route.ts
@@ -1,46 +1,46 @@
-import {NextRequest, NextResponse} from "next/server";
-import {HuggingFace} from "@/services/tti/HuggingFace";
+import { NextRequest, NextResponse } from 'next/server';
+import { HuggingFace } from '@/services/tti/HuggingFace';
 
 type RequestData = {
-    message: string
-    token: string
-}
+  message: string;
+  token: string;
+};
 
 export async function POST(request: NextRequest) {
-    const {message, token}: RequestData = await request.json();
+  const { message, token }: RequestData = await request.json();
 
-    if (!message) {
-        return new NextResponse(
-            JSON.stringify({name: "Please provide something to search for"}),
-            {status: 400}
-        );
-    }
+  if (!message) {
+    return new NextResponse(JSON.stringify({ name: 'Please provide something to search for' }), {
+      status: 400,
+    });
+  }
 
-    try {
-        const response =
-            await fetch(`https://www.google.com/recaptcha/api/siteverify?  
+  try {
+    const response = await fetch(
+      `https://www.google.com/recaptcha/api/siteverify?  
            secret=${process.env.RECAPTCHA_SECRETKEY}&response=${token}`,
-                {
-                    method: 'POST'
-                });
+      {
+        method: 'POST',
+      },
+    );
 
-        await response.json();
-    } catch (e) {
-        console.log(e);
-        return new NextResponse("Capcha validation failed", {
-            status: 400,
-        });
-    }
+    await response.json();
+  } catch (e) {
+    console.log(e);
+    return new NextResponse('Capcha validation failed', {
+      status: 400,
+    });
+  }
 
-    try {
-        const blob = await HuggingFace.execute(message);
-        const headers = new Headers();
-        headers.set("Content-Type", "image/*");
-        return new NextResponse(blob, {status: 200, statusText: "OK", headers});
-    } catch (e) {
-        console.log(e);
-        return new NextResponse("Generation failed", {
-            status: 400,
-        });
-    }
+  try {
+    const blob = await HuggingFace.execute(message);
+    const headers = new Headers();
+    headers.set('Content-Type', 'image/*');
+    return new NextResponse(blob, { status: 200, statusText: 'OK', headers });
+  } catch (e) {
+    console.log(e);
+    return new NextResponse('Generation failed', {
+      status: 400,
+    });
+  }
 }
