11480298746db31f550a3ce9d798705d70d5a7af
Alberto Barboza <73672526+Albertobar94@users.noreply.github.com>
2023-10-06 19:44:10 +0200

    Refactored Replicate class

--- hello-art/src/services/fswap/Replicate.ts
+++ hello-art/src/services/fswap/Replicate.ts
@@ -1,17 +1,20 @@
 import Replicate from 'replicate';
 
 export const FaceSwapper = {
-  async execute(question: string, imgUrl: string) {
+  async execute(
+    targetImg: string,
+    swapImg: string,
+  ) {
     const inference = new Replicate({
       auth: process.env.REPLICATE_API_TOKEN,
     });
 
     return inference.run(
-      'stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf',
+      'lucataco/faceswap:9a4298548422074c3f57258c5d544497314ae4112df80d116f0d2109e843d20d',
       {
         input: {
-          image: imgUrl,
-          question: question,
+          target_image: targetImg,
+          swap_image: swapImg,
         },
       },
     );
